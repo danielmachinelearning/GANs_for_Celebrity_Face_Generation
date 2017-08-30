07081cff5611e27194b70565e28340f38bc51fae
Daniel <daniel@counselytics.com>
2017-08-30 14:17:04 -0400

    First commit of Face Generation Python code plus supporting python scripts.

---
+++ command.sh
@@ -0,0 +1,4 @@
+pip install -r floyd_requirements.txt
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/HtRktiFdQ3mXKVHmz8EMMA' --NotebookApp.token='' --NotebookApp.allow_origin='*'
